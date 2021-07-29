5bd00315e9f9446cc4289c6196a835ea24704d48
joyun <w2039@e-mirim.hs.kr>
2021-07-29 15:21:21 +0900

    temporary calendar.xml and CalendarActivity.java Commit

---
+++ app/src/main/java/kr/hs/emirim/khaproject/DataFormat.java
@@ -0,0 +1,13 @@
+package kr.hs.emirim.khaproject;
+
+import java.util.Date;
+
+public class DataFormat {
+    public DataFormat(String yyyy년_mm월_dd일) {
+
+    }
+
+    public int formate(Date date) {
+        return 0;
+    }
+}
---
+++ app/src/main/java/kr/hs/emirim/khaproject/day.java
@@ -0,0 +1,14 @@
+package kr.hs.emirim.khaproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class day extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_day);
+    }
+}
